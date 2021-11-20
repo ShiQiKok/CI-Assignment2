cfe2b259920a19b664163a0cb20a4208a597adae
wenyihehehe <67746012+wenyihehehe@users.noreply.github.com>
2021-11-20 21:02:55 +0800

    Added testing for layer and activation

--- a2-program.py
+++ a2-program.py
@@ -60,7 +60,7 @@ data = pd.DataFrame(imputer.fit_transform(data), columns=data.columns)
 
 X = data.drop(columns='HeartDisease')
 Y = data['HeartDisease']
-X_train, X_test, y_train, y_test = train_test_split(X, Y, train_size=0.8, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(X, Y, train_size=0.8, random_state=1)
 
 # Scale the input
 scaler = StandardScaler()
@@ -68,15 +68,48 @@ scaler.fit(X_train)
 X_train = scaler.transform(X_train)
 X_test = scaler.transform(X_test)
 
-
-mlp = MLPClassifier(hidden_layer_sizes=(5), max_iter=1000)
+mlp = MLPClassifier(hidden_layer_sizes=(5,), max_iter=1000)
 mlp.fit(X_train, y_train)
 predictions = mlp.predict(X_test)
 print(confusion_matrix(y_test, predictions))
 print(classification_report(y_test, predictions))
 # visualise(mlp)
 
+#Test Parameter (HiddenLayerSize & Activation)
+neuron = 11
+tuples = []
+for i in range(1,neuron+1):
+    tuples.append((i,))
+    for j in range(1,neuron+1):
+        tuples.append((i,j))
+activation=["identity", "logistic", "tanh", "relu"]
 
+#Create ExcelSheet
+import xlsxwriter
+workbook = xlsxwriter.Workbook('result(10).xlsx')
+worksheet = workbook.add_worksheet("Results")
 
+worksheet.write('A1', 'Hidden Layer Sizes')
+worksheet.write('B1', 'Activation')
+worksheet.write('C1', 'Average Accuracy')
 
-
+#Test and write to file
+row = 1
+average = 10
+for t in tuples:
+    for a in activation:
+        averageAccuracy = 0
+        for n in range(average):
+            mlp = MLPClassifier(hidden_layer_sizes=t,activation=a, max_iter=1000)
+            mlp.fit(X_train, y_train)
+            predictions = mlp.predict(X_test)
+            cr=classification_report(y_test, predictions,output_dict=True)
+            accuracy=round(cr["accuracy"],2)
+            averageAccuracy += accuracy
+        averageAccuracy /= average
+        print("Printing result for activation:{activation} and layer:{layer}\nAverage accuracy:{averageAccuracy}".format(activation=a,layer=t,averageAccuracy=averageAccuracy))
+        worksheet.write(row, 0, str(t))
+        worksheet.write(row, 1, a)
+        worksheet.write(row, 2, round(averageAccuracy,2))
+        row += 1
+workbook.close()
